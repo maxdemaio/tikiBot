066d95ebc0a27d6ecffdf0d908afe3790994ed3e
Maxwell DeMaio <maxdman321@gmail.com>
2020-05-26 18:27:50 -0400

    Created ready state return and server join/leave functions

---
+++ bot.py
@@ -0,0 +1,31 @@
+import logging
+import os
+
+import discord
+from discord.ext import commands
+
+import settings
+
+# Create bot instance
+# Set prefix for our commands
+client = commands.Bot(command_prefix = "$")
+
+# Set up logging
+logging.basicConfig(level=logging.INFO)
+
+@client.event
+async def on_ready():
+    print("Bot is ready.")
+
+@client.event
+async def on_member_join(member):
+    """ Bot detects someone has joined a server it is in """
+    print(f"{member} has joined a server.")
+
+@client.event
+async def on_member_remove(member):
+    """ Bot detects someone has left a server it is in """
+    print(f"{member} has left a server.")
+
+
+client.run(os.getenv("TOKEN"))
